b7663637e32d912afe4327ebef36f92a9a393a3a
smalaca <ab13.krakow@gmail.com>
2019-06-28 12:23:00 +0200

    Testing Data - first scenario

---
+++ src/main/java/com/smalaca/CreditGrantedScenario.java
@@ -0,0 +1,7 @@
+package com.smalaca;
+
+public class CreditGrantedScenario {
+    public static final String PESEL = "12345678901";
+    public static final String AMOUNT = "10000";
+    public static final String CREDIT_STATUS = "SUCCESS";
+}
