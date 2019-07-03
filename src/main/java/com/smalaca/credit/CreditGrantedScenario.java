6f252b6692e43aede5d36412e793eae4d75123f3
smalaca <ab13.krakow@gmail.com>
2019-07-03 21:43:27 +0200

    Testing Data - add BIK_STATUS to both scenarios

--- src/main/java/com/smalaca/credit/CreditGrantedScenario.java
+++ src/main/java/com/smalaca/credit/CreditGrantedScenario.java
@@ -4,4 +4,5 @@ public class CreditGrantedScenario {
     public static final String PESEL = "12345678901";
     public static final String AMOUNT = "10000";
     public static final String CREDIT_STATUS = "SUCCESS";
+    public static final String BIK_STATUS = "SUCCESS";
 }
--- src/main/java/com/smalaca/credit/CreditNotGrantedBikInvalidScenario.java
+++ src/main/java/com/smalaca/credit/CreditNotGrantedBikInvalidScenario.java
@@ -4,4 +4,5 @@ public class CreditNotGrantedBikInvalidScenario {
     public static final String PESEL = "22222222222";
     public static final String AMOUNT = "10000";
     public static final String CREDIT_STATUS = "FAIL";
+    public static final String BIK_STATUS = "FAIL";
 }
